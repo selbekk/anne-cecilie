75a57706462de9f4414fe528ec370b259b448eeb
selbekk <kristofer.selbekk@bekk.no>
2014-06-22 22:19:13 +0200

    Update text for 404 page

--- wordpress/wp-content/themes/anne-cecilie/content-none.php
+++ wordpress/wp-content/themes/anne-cecilie/content-none.php
@@ -1,3 +1,10 @@
-<h2>Can't find that page!</h2>
-<p>For some reason, I can't seem to find that page right now.</p>
-<p>Try searching for it using the search field above :-)</p>
+<h1>Wrong way :(</h1>
+<p class="lead">
+    The page or news post you're trying to access, simply doesn't exist. Perhaps you entered
+    the wrong address, or the content is no longer available.
+</p>
+<p class="lead">
+    <strong>That sucks!</strong> Luckily, there are plenty of other cool stuff on
+    this website. Try using the search function or the page navigation to find
+    your way.
+</p>
