a41fce7ae6e156b987d60a581181a42b1e85b295
selbekk <kristofer.selbekk@bekk.no>
2014-06-22 22:58:59 +0200

    Add real 404 page

---
+++ wordpress/wp-content/themes/anne-cecilie/404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<h1>Wrong way :(</h1>
+<p class="lead">
+    The page you're trying to access, simply doesn't exist. Perhaps you entered
+    the wrong address, or the content is no longer available.
+</p>
+<p class="lead">
+    <strong>That sucks!</strong> Luckily, there are plenty of other cool stuff on
+    this website. Try using the search function or the page navigation to find
+    your way.
+</p>
+
+
+<?php get_footer(); ?>
