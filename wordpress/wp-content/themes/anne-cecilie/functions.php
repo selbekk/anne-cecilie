3fd5335566f10d7559cbb2227812f523226c7fa0
selbekk <kristofer.selbekk@bekk.no>
2014-06-25 00:16:51 +0200

    Style gallery functionality

--- wordpress/wp-content/themes/anne-cecilie/functions.php
+++ wordpress/wp-content/themes/anne-cecilie/functions.php
@@ -15,13 +15,66 @@ add_action('init', 'register_menus');
 /* Register content types */
 
 function register_content_types() {
-
+    // TODO: Now, create some content types bro!
 
 }
 add_action('init', 'register_content_types');
 
+
+/* Register custom image sizes for gallery thumbnails */
+add_image_size( 'lg-thumb', '500', '500', true );
+add_image_size( 'lg-thumbnail', '500', '500', true );
+
+/* Override the way the gallery is layed out */
+
+add_filter('post_gallery','customFormatGallery',10,2);
+
+function customFormatGallery($string, $attr){
+
+    $posts = get_posts(array('include' => $attr['ids'],'post_type' => 'attachment'));
+
+    $numColumns = 4; // default value
+    if(isset($attr['columns'])) {
+        $numColumns = abs($attr['columns']);
+    }
+
+    if($numColumns == 0) {
+        $numColumns = 1; // let's not divide by zero
+    };
+
+    $numColumns = ceil(12 / $numColumns);
+
+
+
+    $output = '<div class="gallery-wrapper row">';
+
+    if(isset($attr['heading'])) {
+        $output .= '<h2>'. $attr['heading'] .'</h2>';
+    }
+
+    foreach($posts as $imagePost){
+        //print_r($imagePost); echo "<br /><br />"; // for debugging purposes - remove when working
+        // TODO: style correctly
+        // TODO: respect columns
+        // TODO: create JS for lightbox
+        $imageElem = wp_get_attachment_image($imagePost->ID, 'lg-thumb');
+
+        $output .=  '<div class="gallery-item col-sm-'. $numColumns .'" data-id="'. $imagePost->ID .'">' .
+                        $imageElem .
+                        '<p class="caption">'. $imagePost->post_excerpt .'</p>' .
+                    '</div>';
+    }
+
+    $output .= "</div>";
+
+    return $output;
+}
+
+
+
 /* Registering theme support for stuff */
 
 add_theme_support( 'post-thumbnails' );
+
 
 ?>
